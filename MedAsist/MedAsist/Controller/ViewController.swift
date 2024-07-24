7a7778bd58799774c26f5493f338c6b34bf6f5da
Hassnain Khaliq <macbook@192.168.1.17>
2024-07-24 18:41:51 +0500

    Project init

---
+++ MedAsist/MedAsist/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedAsist
+//
+//  Created by Hassnain Khaliq on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
